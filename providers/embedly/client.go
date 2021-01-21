7f44a585946dbd0fa7018ebb8f4d7d417d8e1f8a
Matt Baer <matt@write.as>
2021-01-21 09:35:03 -0500

    Support setting Embedly key globally in library

--- providers/embedly/client.go
+++ providers/embedly/client.go
@@ -4,10 +4,17 @@ var (
 	Host = "https://api.embed.ly"
 )
 
+// Key is the Embed.ly API key
+var Key string
+
 type Client struct {
 	key string
 }
 
 func NewClient(key string) *Client {
 	return &Client{key}
+}
+
+func DefaultClient() *Client {
+	return &Client{Key}
 }
