1d6411e3b6d02895b637d56de34fb4ef204abdec
Matt Baer <matt@write.as>
2021-02-08 14:38:06 -0500

    Add allowlist of rich media providers

---
+++ providers/providers.go
@@ -0,0 +1,71 @@
+package providers
+
+import (
+	"log"
+	"net/url"
+	"regexp"
+	"strings"
+)
+
+var embedHostDomains = map[string]*regexp.Regexp{
+	"airtable.com":          nil,
+	"www.are.na":            nil,
+	"cinnamon.video":        nil,
+	"codepen.io":            nil,
+	"flic.kr":               nil,
+	"www.flickr.com":        regexp.MustCompile("/photos/.+"),
+	"giphy.com":             regexp.MustCompile("/gifs/.+"),
+	"media.giphy.com":       regexp.MustCompile("/media/.+"),
+	"gph.is":                nil,
+	"gist.github.com":       nil,
+	"gfycat.com":            nil,
+	"imgur.com":             nil,
+	"instagr.am":            regexp.MustCompile("/p/.+"),
+	"www.instagram.com":     regexp.MustCompile("/p/.+"),
+	"www.kickstarter.com":   regexp.MustCompile("/projects/.+/.+"),
+	"quora.com":             nil,
+	"www.quora.com":         nil,
+	"share.getcloudapp.com": nil,
+	"twitter.com":           regexp.MustCompile("/.+/status/.+"),
+	"imgs.xkcd.com":         nil,
+	"xkcd.com":              nil,
+	"www.xkcd.com":          nil,
+	"youtube.ca":            nil,
+	"youtube.jp":            nil,
+	"youtube.com.br":        nil,
+	"youtube.co.uk":         nil,
+	"youtube.nl":            nil,
+	"youtube.pl":            nil,
+	"youtube.es":            nil,
+	"youtube.ie":            nil,
+	"it.youtube.com":        nil,
+	"youtube.fr":            nil,
+	"youtube.com":           nil,
+	"www.youtube.com":       nil,
+}
+
+func EmbedURL(us string) bool {
+	u, err := url.Parse(us)
+	if err != nil {
+		log.Printf("[ERROR] EmbedURL: unable to parse url: %s", err)
+		return false
+	}
+
+	// Ensure domain is from a supported provider
+	ex, domainOK := embedHostDomains[u.Host]
+	domainOK = domainOK ||
+		strings.HasSuffix(u.Host, ".tumblr.com") ||
+		strings.HasSuffix(u.Host, ".typeform.com")
+	if !domainOK {
+		log.Printf("unsupported domain: %s", u.Host)
+		return false
+	}
+
+	if ex == nil {
+		// All URL paths are allowed
+		return true
+	}
+
+	// Check URL path for valid pattern
+	return ex.MatchString(u.Path)
+}
